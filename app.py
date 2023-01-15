f304026e543344d5c973a734e826c66ad9cdf9ce
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-01-15 20:05:59 +0500

    Creating Blueprints
    Creating Blueprints and importing them into app

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
+from api.api import api_bp
+from auth.auth import auth_bp
+from cart.cart import cart_bp
+from general.general import general_bp
+from products.products import products_bp
+
 app = Flask(__name__)
+
+app.register_blueprint(api_bp, url_prefix='/api')
+app.register_blueprint(auth_bp)
+app.register_blueprint(cart_bp, url_prefix='/cart')
+app.register_blueprint(general_bp)
+app.register_blueprint(products_bp, url_prefix='/products')

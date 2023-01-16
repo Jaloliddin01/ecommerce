03a37f07ef92d7c1cc45465efe10b01ae13a3683
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-01-16 10:32:03 +0500

    working on products

--- products/products.py
+++ products/products.py
@@ -6,10 +6,35 @@ products_bp = Blueprint('products_bp', __name__,
     static_folder='static', 
     static_url_path='assets')
 
-# @products_bp.route('/')
-# def list():
-#     products = Product.query.all()
-#     return render_template('products/list.html', products=products)
+@products_bp.route('/')
+def list():
+    products = [
+        {
+            'rasm': 'Pomidor',
+            'nom': 'Pomidor',
+            'narx': 100000,
+            'yaroqlilik': 30
+        },
+        {
+            'rasm': 'Bodiring',
+            'nom': 'Bodiring',
+            'narx': 100000,
+            'yaroqlilik': 30
+        },
+        {
+            'rasm': 'Kartoshka',
+            'nom': 'Kartoshka',
+            'narx': 100000,
+            'yaroqlilik': 90
+        },
+        {
+            'rasm': 'Piyoz',
+            'nom': 'Piyoz',
+            'narx': 100000,
+            'yaroqlilik': 100
+        }
+    ]
+    return render_template('products/list.html', products=products)
 
 # @products_bp.route('/view/<int:product_id>')
 # def view(product_id):
